d32ff3c19b52d6afca72d867857a6ff09b3ac619
Steven Laidlaw <steven.laidlaw@mechanicalrock.io>
2021-07-30 13:44:16 +0800

    Fri 30 Jul 2021 13:44:16 AWST

--- update.sh
+++ update.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-cp ~/.oh-my-zsh/themes/custom.zsh-theme .oh-my-zsh/themes/custom.zsh-theme
-cp ~/.config/i3/config .config/i3/config
-cp ~/.config/i3/scripts/i3lock-custom .config/i3/scripts/i3lock-custom
-cp ~/.config/nvim/init.vim .config/nvim/init.vim
-cp ~/.config/kitty/kitty.conf .config/kitty/kitty.conf
-cp ~/.config/polybar/config .config/polybar/config
-cp ~/.config/polybar/launch.sh .config/polybar/launch.sh
+cp ~/.oh-my-zsh/themes/custom.zsh-theme home/.oh-my-zsh/themes/custom.zsh-theme
+cp ~/.config/i3/config home/.config/i3/config
+cp ~/.config/i3/scripts/i3lock-custom home/.config/i3/scripts/i3lock-custom
+cp ~/.config/nvim/init.vim home/.config/nvim/init.vim
+cp ~/.config/kitty/kitty.conf home/.config/kitty/kitty.conf
+cp ~/.config/polybar/config home/.config/polybar/config
+cp ~/.config/polybar/launch.sh home/.config/polybar/launch.sh
 cp ~/.zshrc home/.zshrc
+cp ~/Pictures/background.png home/Pictures/background.png
 
