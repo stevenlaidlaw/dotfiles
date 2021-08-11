ab8f269c98fb5825f9054aebc2a8f3556b7372fb
Steven Laidlaw <steven.laidlaw@mechanicalrock.io>
2021-08-11 08:14:18 +0800

    Wed 11 Aug 2021 08:14:18 AWST

--- home/.config/polybar/launch.sh
+++ home/.config/polybar/launch.sh
@@ -7,6 +7,14 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar-example.log
-polybar example 2>&1 | tee -a /tmp/polybar-example.log & disown
+
+if type "xrandr"; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		MONITOR=$m polybar --reload example &
+	done
+else
+	polybar --reload example &
+	# polybar example 2>&1 | tee -a /tmp/polybar-example.log & disown
+fi
 
 echo "Bars launched..."
