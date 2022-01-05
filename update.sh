edb27de6b6c5af4bd9016b82a63e975454a1809d
Steven Laidlaw <steven.laidlaw@mechanicalrock.io>
2022-01-05 09:49:03 +0800

    Wed 05 Jan 2022 09:49:03 AWST

--- update.sh
+++ update.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 cp ~/.oh-my-zsh/themes/custom.zsh-theme home/.oh-my-zsh/themes/custom.zsh-theme
-cp ~/.config/i3/config home/.config/i3/config
-cp ~/.config/i3/scripts/i3lock-custom home/.config/i3/scripts/i3lock-custom
 cp ~/.config/nvim/init.vim home/.config/nvim/init.vim
 cp ~/.config/kitty/kitty.conf home/.config/kitty/kitty.conf
 cp ~/.config/kitty/kitty-themes/themes/Custom.conf home/.config/kitty/kitty-themes/themes/Custom.conf
-cp ~/.config/polybar/config home/.config/polybar/config
-cp ~/.config/polybar/launch.sh home/.config/polybar/launch.sh
-cp ~/.config/polybar/spotify.py home/.config/polybar/spotify.py
 cp ~/.zshrc home/.zshrc
 cp ~/Pictures/Background.png home/Pictures/Background.png
+cp ~/Pictures/Background.xcf home/Pictures/Background.xcf
 
