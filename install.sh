d06fa10147dfa7515e527bfe4c5333ce8d11c89e
Steven Laidlaw <steven@stevenlaidlaw.com>
2022-08-12 10:30:55 +0800

    Fri 12 Aug 2022 10:30:55 AWST

--- install.sh
+++ install.sh
@@ -12,4 +12,4 @@ nvim +'PlugInstall --sync' +qa
 # Install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # Install default .tmux config
-git clone https://github.com/gpakosz/.tmux && ln -s -f .tmux/.tmux.conf && cp .tmux/.tmux.conf.local .
+git clone https://github.com/gpakosz/.tmux ~/.tmux && ln -s -f .tmux/.tmux.conf ~/.tmux.conf && cp ~/.tmux/.tmux.conf.local ~/
