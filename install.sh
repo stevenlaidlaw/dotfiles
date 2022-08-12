70758105d9317e3112397230681cb6a9f3143b41
Steven Laidlaw <steven@stevenlaidlaw.com>
2022-08-12 10:23:13 +0800

    Fri 12 Aug 2022 10:23:13 AWST

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 # Always want to use ZSH as my default shell (e.g. for SSH)
-if ! grep -q "root.*/bin/zsh" /etc/passwd
-then
-  chsh -s /bin/zsh root
-fi
+sudo chsh -s /bin/zsh vscode
 
 rm **/.DS_Store
 # Sync dotfiles
