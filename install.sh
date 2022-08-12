4993c9b21822f3f797d51076604c3fadc35bbe04
Steven Laidlaw <steven@stevenlaidlaw.com>
2022-08-12 08:06:49 +0800

    Fri 12 Aug 2022 08:06:49 AWST

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 rm **/.DS_Store
 # Sync dotfiles
 rsync -aivP home/ ~/
+# Set default shell to ZSH
+chsh -s $(which zsh)
 # Install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 nvim +'PlugInstall --sync' +qa
@@ -8,5 +10,3 @@ nvim +'PlugInstall --sync' +qa
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # Install default .tmux config
 git clone https://github.com/gpakosz/.tmux.git && ln -s -f .tmux/.tmux.conf && cp .tmux/.tmux.conf.local .
-# Set default shell to ZSH
-chsh -s $(which zsh)
