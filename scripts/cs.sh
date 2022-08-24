440d21c8555d31a21355e231d04922ea839893d7
Steven Laidlaw <steven@stevenlaidlaw.com>
2022-08-24 08:17:46 +0800

    Wed 24 Aug 2022 08:17:46 AWST

--- scripts/cs.sh
+++ scripts/cs.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
-codespace="$CS_NAME"
+if [ -z "$1" ]; then
+		echo "Usage: $0 <codespace>"
+		echo "`codespace` finds the first available codespace that matches the given pattern"
+		exit 1
+fi
 
+codespace=`gh cs list | grep "$1" | head -n 1 | awk '{print $1}'`
+
+if [ -z "$codespace" ]; then
+		echo "No codespace found"
+		exit 1
+fi
+
+echo "Forwarding port 80"
 gh cs ports forward 80:80 -c "$codespace" &
 pid=$!
 
+echo "Attempting to connect to codespace $codespace"
 gh cs ssh -c "$codespace"
 
 kill -s TERM "$pid"
-echo "Done"
+echo "Killed codespace port forwarding ($pid)"
