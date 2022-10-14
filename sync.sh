fe9752adb9bfc9268466bf4c5df9b3d0b204bee1
Steven Laidlaw <steven@stevenlaidlaw.com>
2022-10-14 09:02:13 +0800

    Fri 14 Oct 2022 09:02:13 AWST

--- sync.sh
+++ sync.sh
@@ -1,6 +1,7 @@
 cp ~/.oh-my-zsh/themes/custom.zsh-theme home/.oh-my-zsh/themes/custom.zsh-theme
 cp ~/.config/nvim/init.vim home/.config/nvim/init.vim
 cp ~/.zshrc home/.zshrc
+cp ~/.tmux.conf.local home/.tmux.conf.local
 cp ~/.vale.ini home/
 cp ~/.config/alacritty.yml home/.config/
 cp -r ~/.config/kitty home/.config/
