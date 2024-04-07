5b909db263d7aa10b813a08248290d61c84e8a70
Steven Laidlaw <steven@stevenlaidlaw.com>
2024-04-08 06:22:53 +0800

    Mon Apr  8 06:22:53 AWST 2024

--- local/install.sh
+++ local/install.sh
@@ -7,23 +7,19 @@ sudo chsh -s /bin/zsh $(whoami)
 # Install zsh addons
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-rsync -aivP home/.zshrc ~/
+# Install applications
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install node
+nvm use node
 
-
-if [[ "$CODESPACES" != "true" ]]; then
-	git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-	# Install nvm
-	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-	export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-	nvm install node
-	nvm use node
-	 #Sync dotfiles
-	rsync -aivP home/ ~/
-	# Install prettier
-	npm i --global prettier
-	# Install yarn
-	npm i --global yarn
-fi
+#Sync dotfiles
+rsync -aivP home/ ~/
+# Install prettier
+npm i --global prettier
+# Install yarn
+npm i --global yarn
 
 echo "Done installing dotfiles!"
