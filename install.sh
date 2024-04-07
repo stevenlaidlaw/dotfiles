01845550770c239c7ea4f0050d4a0eb4d9c94458
Steven Laidlaw <steven@stevenlaidlaw.com>
2024-04-08 06:11:14 +0800

    Mon  8 Apr 2024 06:11:14 AWST

--- install.sh
+++ install.sh
@@ -6,24 +6,24 @@ sudo chsh -s /bin/zsh $(whoami)
 
 # Install zsh addons
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-# Install alacritty terminfo
-wget https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.info && tic -xe alacritty,alacritty-direct alacritty.info && rm alacritty.info
 
-# Rewrite this to use asdf
-# Install nvm
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-#export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-#nvm install node
-#nvm use node
+rsync -aivP home/.zshrc ~/
 
-#git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 
- #Sync dotfiles
-rsync -aivP home/ ~/
-# Install prettier
-#npm i --global prettier
-# Install yarn
-#npm i --global yarn
+if [[ "$CODESPACES" != "true" ]]; then
+	git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+	# Install nvm
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+	export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+	nvm install node
+	nvm use node
+	 #Sync dotfiles
+	rsync -aivP home/ ~/
+	# Install prettier
+	npm i --global prettier
+	# Install yarn
+	npm i --global yarn
+fi
 
 echo "Done installing dotfiles!"
