14e5c05ff499879501872dec8e8536f338b40957
Steven Laidlaw <steven@stevenlaidlaw.com>
2024-04-12 07:50:29 +0800

    Fri Apr 12 07:50:29 AWST 2024

--- sync.sh
+++ sync.sh
@@ -1,7 +1,13 @@
 # Get local files and put them in this repo
 
-files_array=(~/.oh-my-zsh/themes/custom.zsh-theme ~/.zshrc ~/.vimrc ~/.config/kitty ~/.fonts)
+files_array=(
+  .oh-my-zsh/themes/custom.zsh-theme
+  .zshrc
+  .vimrc
+  .config/kitty/
+  .fonts/
+)
 
 for file in ${files_array[@]}; do
-  rsync -aivP $file home/
+  rsync -aivP --mkpath ~/$file home/$file
 done
